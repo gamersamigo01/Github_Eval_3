188d57c13c63dda5f0c6a08ab9bb7739dc59f006
Luciano Gomez <luciano.gomezv01@gmail.com>
2026-05-09 14:30:12 -0400

    Ejercicio 2 de guia

---
+++ Estudio de Arrays/Guia_2.py
@@ -0,0 +1,30 @@
+#lista super
+sw = 1
+lista_super = []
+valor_super = []
+print("Precione 1 para ingresar los productos del super")
+print("Precione cualquier tecla para salir")
+
+opt = int(input("Ingrese su opcion: "))
+
+if opt == 1:
+    while sw ==1:
+        try:
+            print("-"*16)
+            producto = input("Ingrese sus productos, para salir ingrese 0: ")
+            if producto != "0":
+                lista_super.append(producto)
+                valor_producto = int(input("Ingrese el valor del (producto): "))
+                valor_super.append(valor_producto)
+                print("----DETALLE DE LA BOLETA----")
+                print(f"Sus productos son: {lista_super}")
+                print(f"Usted ingreso: {len(lista_super)} productos")
+                print(f"El precio total es de: {sum(valor_super)}")
+            else:
+                print("Gracias")
+                sw = 0
+        except:
+            print("Ingreso erroneo")
+else:
+    print("Adios")
+    
