fae20a8ba68334c1fac93c7ad57464c293c0219e
Luciano Gomez <luciano.gomezv01@gmail.com>
2026-05-09 13:31:25 -0400

    Estudio de listas

---
+++ Estudio de listas/Estudio_1.py
@@ -0,0 +1,24 @@
+# milista = [1,2,3,4,5]
+# for elemento in milista:
+#     print(elemento)
+
+# milista = [1,2,3,4,5]
+# print(milista[2])
+
+# milista = [1,2,3,4,5]
+# milista.append(6)
+
+# for elemento in milista:
+#     print(elemento)
+
+# milista = [1,2,3,4,5]
+# milista.insert(3,"juan")
+
+# for elemento in milista:
+#     print(elemento)
+
+# milista = [1,2,3,"juan",4,5]
+# milista.remove("juan")
+# for elementos in milista:
+#     print(elementos)
+
