c2616dce9ef49b31dd51d7938d97f32e6896df49
Luciano Gomez <luciano.gomezv01@gmail.com>
2026-05-11 11:14:11 -0400

    arreglos ejercicio 5

--- Estudio de ciclos/Ejercicio_5.py
+++ Estudio de ciclos/Ejercicio_5.py
@@ -1,13 +1,19 @@
-numero1 = int(input("Ingrese el primer numero: "))
-numero2 = int(input("Ingrese el segundo numero: "))
+
 
 resultado = 0
 try:
+    numero1 = int(input("Ingrese el primer numero: "))
+    numero2 = int(input("Ingrese el segundo numero: "))
+    try:
+        resultado = numero1 // numero2
+    except ZeroDivisionError:
+        while numero2 == 0:
+            print("No se puede dividir por 0")
+            numero2 = int(input("Ingrese el segundo numero nuevamente: "))
+        resultado = numero1 // numero2
+except ValueError:
+    print("Lo que se ingresa debe ser numerico")
+    numero1 = int(input("Ingrese el primer numero: "))
+    numero2 = int(input("Ingrese el segundo numero: "))
     resultado = numero1 // numero2
-except ZeroDivisionError:
-    while numero2 == 0:
-        print("No se puede dividir por 0")
-        numero2 = int(input("Ingrese el segundo numero nuevamente: "))
-    resultado = numero1 // numero2
-
 print(f"El resultado de su division es: {resultado}")
