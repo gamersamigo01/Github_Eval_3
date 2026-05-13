e7ea78c86de76fce7fb9d27a2fb370d22e3ea5a3
Luciano Gomez <luciano.gomezv01@gmail.com>
2026-05-13 17:31:59 -0400

    Update Ejercicio_8.py

--- Estudio de ciclos/Guia 1/Ejercicio_8.py
+++ Estudio de ciclos/Guia 1/Ejercicio_8.py
@@ -2,11 +2,14 @@ pasajes = int(input("¡Cuantos pasajes va a vender?: "))
 
 totalingresos = 0
 
-for i in range(pasajes+1):
+for i in range(pasajes):
     try:
         costo = int(input("Ingrese el coste del pasaje: "))
         totalingresos = costo + totalingresos
     except ValueError:
+        while not costo.isdigit():
             print("Error, Tiene que ser un digito")
+            costo = int(input("Ingrese el coste del pasaje: "))
+            
             
 print(f"El total de las ventas de los pasajes es de: {totalingresos}")
