3a9ef6b3ca2b80d139aca5f0f2ffc8f550b862e8
Luciano Gomez <luciano.gomezv01@gmail.com>
2026-05-13 22:01:45 -0400

    Ejercicio 8

--- Estudio de ciclos/Guia 2/Ejercicio_7.py
+++ Estudio de ciclos/Guia 2/Ejercicio_7.py
@@ -1,7 +1,8 @@
-for i in range(2,31):
+for i in range(2,31): #Se recorre la lista completa hasta 30
     es_primo = True
 
-    for divisor in range(2,i):
+    for divisor in range(2,i): #Aca se recorren todos los numeros antes de i, por ejemplo si i es 5, recorre 2,3,4 y los divide 5./
+                               #si alguno de estos da == 0, significan que no son primos pues se puede dividir por otro numero que no sea 5 y 1
         if i % divisor == 0:
             es_primo = False
             break
---
+++ Estudio de ciclos/Guia 2/Ejercicio_8.py
@@ -0,0 +1,7 @@
+num = 7
+if num > 0:
+    for i in range(1,11):
+        multi = 7 * i
+        print(f"7 x {i} = {multi}")
+else:
+    print("El numero no puede ser menor o igual a 0")
