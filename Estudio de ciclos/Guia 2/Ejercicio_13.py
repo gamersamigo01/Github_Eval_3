5f5fad0f2f6724d82dd2072a0dc3f8e41fcb0d4e
Luciano Gomez <luciano.gomezv01@gmail.com>
2026-05-14 12:00:12 -0400

    Update Ejercicio_13.py
    Asi es

--- Estudio de ciclos/Guia 2/Ejercicio_13.py
+++ Estudio de ciclos/Guia 2/Ejercicio_13.py
@@ -1,7 +1,15 @@
-lista_notas = []
-
-nota = int(input("Ingrese sus notas: "))
-while nota != 0:
-    lista_notas.append(nota)
-    nota = int(input("Ingrese sus notas: "))    
-print(lista_notas)
+clave_correcta = "Python123"
+clave_intento = ["Hola","123", "Python123"]
+i = 0
+acceso = True
+while i < 3 and i < len(clave_intento): #i por si solo es el contador, y el i < len(Clave_intento) sirve para que el contador sea menor al total de palabras puestas
+    if clave_intento[i] != clave_correcta:
+        i += 1
+        print("Clave incorrecta")
+        acceso = False
+        
+    else:
+        print("Clave correcta")
+        acceso = True
+        break
+print(f"¿Tiene acceso?: {acceso}")
