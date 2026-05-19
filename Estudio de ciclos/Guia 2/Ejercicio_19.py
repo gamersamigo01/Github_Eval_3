9e69f1408c90d033deed6d4bd2f2701452ec7766
Luciano Gomez <luciano.gomezv01@gmail.com>
2026-05-18 20:05:31 -0400

    Create Ejercicio_19.py

---
+++ Estudio de ciclos/Guia 2/Ejercicio_19.py
@@ -0,0 +1,7 @@
+correos = ["Luciano.gomez","Javieraraneda@pan","Tomasgonzales@gmail.com"]
+i = 0
+for i in correos:
+    if "@" in i:
+        print("Correo correcto")
+    else:
+        print("Correo incorrecto")
