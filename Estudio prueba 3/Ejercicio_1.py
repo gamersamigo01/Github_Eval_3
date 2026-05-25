670052caf811d0dbde7aec766d6e54506d7c63a5
Luciano Gomez <luciano.gomezv01@gmail.com>
2026-05-25 10:29:20 -0400

    ejercicio 1

--- Estudio prueba 3/Ejercicio_1.py
+++ Estudio prueba 3/Ejercicio_1.py
@@ -6,27 +6,30 @@ except ValueError:
     print("Cantidad invalida!, Ingrese un entero positivo para continuar")
 else:
     if cuant_auto > 0:
-        for auto in range(0,cuant_auto):
-            while placa_valida and auto <= cuant_auto:
+        for auto in range(cuant_auto):
+            while True:
                 placa = input(f"Ingrese la placa del {auto+1}° auto: ")
                 if (len(placa) >= 6) and not (" " in placa):
-                    auto += 1
                     print(f"Placa valida {placa}")
-                    placa_valida = False
+                    break
                 else:
-                    placa = input("Error, Ingrese nuevamente la placa:")
-                    placa_valida = True
-
-        for capacidad in range(0,cuant_auto):
-            capacidad_auto = int(input(f"Ingrese la capacidad del {capacidad+1}° auto: "))
-            if capacidad_auto > 0:
-                print(f"Capacidad registrada")  
-                if capacidad_auto > 55:
-                    sum_pesado += 1
-                elif capacidad_auto <= 55:
-                    sum_liviano += 1
-            else: 
-                print("Error, la capacidad debe ser mayor a 0") 
+                    print("Error, la placa tiene que tener al menos 6 caracteres y no tener espacios")
+                    
+            while True:
+                try:
+                    capacidad_auto = int(input(f"Ingrese la capacidad del {auto+1}° auto: "))
+                except ValueError:
+                    print("Error, ingrese un numero entero positivo")
+                else:
+                    if capacidad_auto > 0:
+                        print(f"Capacidad registrada")  
+                        if capacidad_auto > 55:
+                            sum_pesado += 1
+                        else:
+                            sum_liviano += 1
+                        break
+                    else: 
+                        print("Error, la capacidad debe ser mayor a 0") 
     else:
         print("Error, tiene que ser mayor que cero")
     print(f"La flota cuenta con {sum_pesado} vehiculos pesados y {sum_liviano} vehiculos livianos! ¡Rutas asignadas!")  
