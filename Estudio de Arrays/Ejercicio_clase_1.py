3c283246b906f9557324c75be28b733f94b0157c
Luciano Gomez <luciano.gomezv01@gmail.com>
2026-05-28 12:14:27 -0400

    ta

--- Estudio de Arrays/Ejercicio_clase_1.py
+++ Estudio de Arrays/Ejercicio_clase_1.py
@@ -1,13 +1,19 @@
-colores = ["rojo","verde","amarillo","morado","negro","azul"]
-valor_encontrado = False
+# colores = ["rojo","verde","amarillo","morado","negro","azul"]
+# valor_encontrado = False
 
-color_a_buscar = input("Ingrese un color: ").lower().strip()
+# color_a_buscar = input("Ingrese un color: ").lower().strip()
+# for indice,color in enumerate(colores):
+#     if color == color_a_buscar:
+#         print(f"{indice+1}- El color es: {color}")
+#         valor_encontrado = True
+#         break           
+# if not valor_encontrado:
+#     print("El valor no fue encontrado")
+            
+colores = ["rojo","verde","amarillo"]
+color_a_buscar = input("Ingrese un color: ").strip().lower()
+# colores.append(color_a_buscar)
+colores.insert(1,color_a_buscar)
+
 for indice,color in enumerate(colores):
-    if color == color_a_buscar:
-        print(f"{indice+1}- El color es: {color}")
-        valor_encontrado = True
-        break           
-if not valor_encontrado:
-    print("El valor no fue encontrado")
-            
-            
+    print(f"{indice+1}° el color es: {color}")
